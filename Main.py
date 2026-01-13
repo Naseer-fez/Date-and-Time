937a9f58bf963a014199b52872e29fbe296530cc
Naseer <feznaseer777@gmail.com>
2026-01-13 11:31:37 +0530

    THe main python file with Day-1 progress

---
+++ Main.py
@@ -0,0 +1,71 @@
+import ctypes as C
+import os
+
+
+# clib=C.CDLL(os.path.join(path,'array.dll'))
+
+class Date_Time:
+    def __init__(self):
+        self.path=os.getcwd()
+        # self.memoryalocating_date(1)
+
+        pass
+    def memoryalocating_date(self,format):
+        """This function is used to allocate the space for the array 
+        which is used to store the date , the default is one """
+        mr=C.CDLL(os.path.join(self.path,'memory_Date.dll'))
+        memory=mr.allocatiing_memory_date
+        memory.argtypes=[C.c_int]
+        memory.restype=C.POINTER(C.c_int)
+        format=C.c_int(format)
+        return  memory(format) #return the memory address
+    """so the format which i have decide is , if it is 1 which is the default iam using the indian time stand
+    Day -Month-Year and if it is 2 then the american format , Month-day -year,in future i think i can get the 3rd foramt which is the 
+     Year-month -day, depending on the liking  """
+    def Date_Checker(self,date:str):
+        if not isinstance(date,str):
+            raise TypeError("The Format which you have mentioned is not a string and this is not valid,")
+        """The Hierachary fill is-->
+        First year, Second is month and the last is date, it is always the present year followed by the decade """
+        #now lets use the c function
+        dllpath=C.CDLL(os.path.join (self.path,"Datecheck.dll"))
+        checker=dllpath.Dateforamtchecker
+        checker.argtypes=[C.c_char_p,C.c_int]
+        checker.restype=C.c_int
+        passingdate=date.replace("-", "")
+        passingdate=passingdate.encode("UTF-8")
+        value=checker(passingdate,len(passingdate))
+        if (value==0):
+            raise TypeError("This is not the falid way the Date Contais a charater ")
+        self.date=date
+        # print(self.date)
+    def Date(self,date:str,format=1):
+        if format!=(1 or 0):
+            raise TypeError("The format specided is not available")
+
+
+        self.Date_Checker(date)
+        date_arr=self.memoryalocating_date(format)
+        dllpath=C.CDLL(os.path.join (self.path,"Date_Filler.dll"))
+        date_maker=dllpath.datearrangment
+        date_maker.argtypes=[C.c_char_p,C.POINTER(C.c_int)]
+        date_maker.restype=None
+        date=date.replace("-", "").encode("UTF-8")#this can be optmised cause i call this again in the above line
+        date_maker(date,date_arr)
+        # for i in range (9):
+        #     print(C.c_int(date_arr[i]))
+        # pass
+        """Untill this part the memory allaoction and date transfer is done , now i need to 
+        make or add function this like , subration of the year, days , months ,etc, i have to add another format method aslo """
+
+
+
+
+    
+
+        
+
+
+if __name__=="__main__":
+   z=Date_Time()
+z.Date("12-06-2005")
